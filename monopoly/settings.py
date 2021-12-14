0a2e3046955dbc4c4181dbb9587d8d6309ce1bcf
kiruthihan nagarajan <47429159+kiruthihan10@users.noreply.github.com>
2021-12-14 16:18:22 +0530

    static app removed

--- monopoly/settings.py
+++ monopoly/settings.py
@@ -36,7 +36,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
     'transactions',
@@ -123,7 +122,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
